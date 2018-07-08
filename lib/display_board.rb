6d14c9a738fb6b36467fff7db444185277887f6a
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 19:02:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(tl: " ", tm: " ", tr: " ", ml: " ", mm: " ", mr: " ", bl: " ", bm: " ", br: " ")
+  puts " #{:tl}"
+end
