f044998ca46b358a5fe99fb64ace771211f28dc3
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 22:32:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,32 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-ROWLINE = "\n-----------\n"
+ROWLINE = "-----------"
 
 def display_board(b = [" "," "," "," "," "," "," "," "," "])
-  row_num = 1
-  b.each_index do |i|
-    puts "#{i}:"
-    unless row_num == 5
-      unless row_num % 2 == 0
-        unless (i + 1) % 3 == 0
-          print " #{b[i]} |"
-        else
-          print " #{b[i]} "
-          row_num += 1
-        end
-      else
-        print ROWLINE
-        row_num += 1
-      end
-    else
-      unless (i + 1) % 3 == 0
-        print " #{b[i]} |"
-      else
-        print " #{b[i]} "
-      end
-    end
-  end
+  puts " #{b[0]} | #{b[1]} | #{b[2]} "
+  puts ROWLINE
+  puts " #{b[3]} | #{b[4]} | #{b[5]} "
+  puts ROWLINE
+  puts " #{b[6]} | #{b[7]} | #{b[8]} "
 end
 
 display_board(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
