15262f760f2a304175d8cb5c879693f6fcaf4e0a
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 21:54:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -31,6 +31,34 @@ end
 
 display_board(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
+# ROWLINE = "\n-----------\n"
+
+def display_board(b = [" "," "," "," "," "," "," "," "," "])
+  row_num = 1
+  b.each_index do |i|
+    puts "#{i}:"
+    unless row_num == 5
+      unless row_num % 2 == 0
+        unless (i + 1) % 3 == 0
+          print " #{b[i]} |"
+        else
+          print " #{b[i]} "
+          row_num += 1
+        end
+      else
+        print ROWLINE
+        row_num += 1
+      end
+    else
+      unless (i + 1) % 3 == 0
+        print " #{b[i]} |"
+      else
+        print " #{b[i]} "
+      end
+    end
+  end
+end
+#
 # def display_board(ary)
 #  spot = 1
 #  board = {}
