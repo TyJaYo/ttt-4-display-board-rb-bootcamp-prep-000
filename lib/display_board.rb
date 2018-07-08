7a05d67c770d8bd474263a192f6225125094f82c
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 19:41:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = {}
 
-def display_board(tl: " ", tm: " ", tr: " ", ml: " ", mm: " ", mr: " ", bl: " ", bm: " ", br: " ")
+def display_board(*args)
+  spot = 1
+  args.each do |a|
+    
+    board <<
   board = [:tl, :tm, :tr, :ml, :mm, :mr, :bl, :bm, :br]
   puts " #{tl}"
 end
 
 display_board(["O", "O", "O", "O", "O", "O", "O", "O", "O"])
 puts board
+
+# tl: " ", tm: " ", tr: " ", ml: " ", mm: " ", mr: " ", bl: " ", bm: " ", br: " "
