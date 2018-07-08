e26759a2c45e4ae7b72fe6100d471ff43066a713
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 19:11:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(tl: nil)
+def display_board(tl: 1)
   board = [:tl, :tm, :tr, :ml, :mm, :mr, :bl, :bm, :br]
   puts " #{:tl}"
 end
